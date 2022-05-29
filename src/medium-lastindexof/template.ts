74834f36ea46482c8a9d4719fd9e78de29b0d902
Alex <2230078305@qq.com>
2022-05-29 14:49:01 +0800

    chore

--- src/medium-lastindexof/template.ts
+++ src/medium-lastindexof/template.ts
@@ -1,7 +1,9 @@
 type MyEqual<A, B> = (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? true : false;
-type Sub1<A extends any[]> = A extends [any, ...infer AR1] ? AR1 : []
-type LastIndexOf<T extends any[], U extends number, Copy extends any[] = T> = T extends [...infer R1, infer R2]
-  ? MyEqual<R2, U> extends true 
-    ? Sub1<Copy>['length'] 
-    : LastIndexOf<R1, U, Sub1<Copy>>
+
+type Sub<T extends any[]> = T extends [any, ...infer R] ? R : []
+
+type LastIndexOf<T extends any[], U, R extends any[] = T> = T extends [...infer F, infer L]
+  ? MyEqual<L, U> extends true 
+    ? Sub<R>['length'] 
+    : LastIndexOf<F, U, Sub<R>>
   : -1
